21f6adccedd8b93c6ba1f674759226f575c6083a
Rasmus Steen Hofmann Nielsen <9pro123@gmail.com>
2021-01-09 22:02:31 +0100

    update

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from Login.login import login
+from Login.register import register
 
 browser = webdriver.Chrome(executable_path="Main/chromedriver.exe")
-
 browser.get('https://www.zravian.com/')
 
-#register(browser, 'nielsen', 'monster', 'rasm2211@gmail.com', 'roman', '-+')
 
+# Create account: 1:Username 2:Password 3:Mail 4:Tribe 5:location 6:Referrer(not needed) !Remember('')!
+register(browser, 'nielsen', 'monster', 'rasm2211@gmail.com', 'roman', '-+')
+
+# login: 1:Username 2:password
 login(browser, 'nielsen', 'monster')
